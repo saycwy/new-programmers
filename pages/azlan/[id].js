2298263ff608cee413e1e7f7204dfdf1e54075c6
LAPTOP-0B3TUR6J\Asterisk <mohamad.azlan@asterisk-bs.com>
2021-10-07 17:14:47 +0800

    try delete

---
+++ pages/azlan/[id].js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const CustomerList = () => {
+  const router = useRouter();
+  const [id, setId] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+      if (router.query.id) {
+        setId(router.query.id);
+      }
+  }, [router.query.id]);
+
+  useEffect(() => {
+    _getData();
+  }, [id]);
+
+  const _getData = async () => {
+    let c = await axios.get(`/api/customer/${id}`);
+    // console.log("c: ", JSON.stringify(c.data, null, 2));
+    if (c.statusText === "OK") {
+      setData(c.data);
+    }
+  }
+
+  const _deleteData = async () => {
+    
+  }
+
+  return (
+    <div style={{ padding: 10 }}>
+      <h1>Customer Details</h1>
+      <hr></hr>
+      <div>
+          <button
+            onClick={() => {
+              router.push("/azlan/customerlist");
+            }}
+          >
+            Back
+          </button>
+          <p></p>
+        </div>
+      <div>
+      Customer Name: {data.cust_name}
+      </div>
+    </div>
+  );
+};
+
+export default CustomerList;
---
+++ pages/azlan/customerform.js
@@ -0,0 +1,78 @@
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const CustomerForm = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    // console.log(JSON.stringify(data, null, 2));
+
+    // let body = data;
+    let body = {
+      cust_name: data.cust_name,
+      phone_no: data.phone_no,
+      business_reg_no: data.business_reg_no,
+      business_reg_dd: data.business_reg_dd,
+      pic_name: data.pic_name,
+    };
+
+    // insert data
+    let res = await axios.post(`/api/customers`, body);
+
+    if (res.status === 200) {
+      alert(res.data);
+    } else {
+      alert("Failed.");
+    }
+  };
+
+  return (
+    <div>
+      <div style={{ padding: 10 }}>
+        <h1>Customer Form</h1>
+        <hr></hr>
+        <div>
+          <button
+            onClick={() => {
+              router.push("/azlan/customerlist");
+            }}
+          >
+            Back
+          </button>
+          <p></p>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <input {...register("cust_name")} placeholder="Customer Name" />
+          </div>
+          <div>
+            <input {...register("phone_no")} placeholder="Phone No" />
+          </div>
+          <div>
+            <input
+              {...register("business_reg_no")}
+              placeholder="Business Registration No"
+            />
+          </div>
+          <div>
+            <input
+              {...register("business_reg_dd")}
+              placeholder="Business Registration Date"
+            />
+          </div>
+          <div>
+            <input {...register("pic_name")} placeholder="Person In Charged" />
+          </div>
+          <div>
+            <p></p>
+            <button type="submit">Save/Modify</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerForm;
